6440610871be274081e229759ef330ba19a92ad6
VertOurs <vertours.dev@gmail.com>
2022-05-04 17:27:12 +0200

    eureka + quotingService

---
+++ QuotingService/src/test/java/fr/vertours/quotingservice/QuotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vertours.quotingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
