fe790b16e817f9606497be487561fb22a224066d
VertOurs <vertours.dev@gmail.com>
2022-05-25 11:27:57 +0200

    prometheus mise en place

--- PrometheusTest/PrometheusTest/src/main/java/fr/vertours/PrometheusTest/MonitoringConfiguration.java
+++ PrometheusTest/PrometheusTest/src/main/java/fr/vertours/PrometheusTest/MonitoringConfiguration.java
@@ -1,2 +1,15 @@
-package fr.vertours.PrometheusTest;public class MonitoringConfiguration {
+package fr.vertours.PrometheusTest;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringConfiguration {
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
--- PrometheusTest/PrometheusTest/src/main/java/fr/vertours/PrometheusTest/PrometheusController.java
+++ PrometheusTest/PrometheusTest/src/main/java/fr/vertours/PrometheusTest/PrometheusController.java
@@ -1,4 +1,15 @@
 package fr.vertours.PrometheusTest;
 
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PrometheusController {
+
+    @Timed(value = "greeting.time", description = "Time taken to return greeting")
+    @GetMapping("/")
+    public String hello() {
+        return "hello world";
+    }
 }
